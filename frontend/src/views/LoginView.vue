66d95eb5ccd7a29a0425618abdc745d7e69029d0
e.agudelo <e.agudelo@woden.com.co>
2026-03-03 09:07:40 -0500

    Ajustes de diseño

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -4,14 +4,6 @@
 
     <div class="woden-card relative" :class="isDark ? 'card-dark' : 'card-light'">
 
-      <div
-        class="absolute -top-4 -right-2 w-11 h-11 bg-gradient-to-tr from-[#ff8f00] to-[#ff4e00] rounded-2xl flex items-center justify-center shadow-xl rotate-12">
-        <svg class="w-5 h-5 text-white -rotate-12" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-            d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z" />
-        </svg>
-      </div>
-
       <div>
         <div class="flex items-center gap-2 mb-8">
           <span class="relative flex h-2 w-2">
